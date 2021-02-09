1b67dc0e873db44432f067d334f97c4272c2d823
Ilya Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-02-09 11:11:55 +0300

    Created CqrsFramework and UseCases projects.

---
+++ CqrsFramework/IRequestHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CqrsFramework
+{
+    public interface IRequestHandler<TRequest, TResponse>
+    {
+        Task<TResponse> HandleAsync(TRequest request);
+    }
+}
